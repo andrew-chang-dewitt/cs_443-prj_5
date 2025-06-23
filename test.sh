ea8498c1592124b2df50059fced5b37b40dbcb4c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-06-23 14:33:05 -0500

    chore: clean up whitespace

--- test.sh
+++ test.sh
@@ -12,23 +12,20 @@ do
     progr=`echo $progrr | sed 's/Result: \(.*\)/\1/g'`
     if [[ ${r:0:1} = "E" ]]
     then
-	rv=${r:1:2}
-	if [[ $progret -eq $rv ]]
-	then
-	    echo -ne '\033[0;32mPASSED\033[0m'
-	else
-	    echo -ne "\033[0;31mFAILED\033[0m: Expected: Error"
-	fi
-    else
-	if [[ $r = $progr ]]
-	then
-	    echo -ne '\033[0;32mPASSED\033[0m'
-	else
-	    echo -ne "\033[0;31mFAILED\033[0m: Expected: $r, Got: $progr"
-	fi
+        rv=${r:1:2}
+        if [[ $progret -eq $rv ]]
+        then
+            echo -ne '\033[0;32mPASSED\033[0m'
+        else
+            echo -ne "\033[0;31mFAILED\033[0m: Expected: Error"
+        fi
+          else
+        if [[ $r = $progr ]]
+        then
+            echo -ne '\033[0;32mPASSED\033[0m'
+        else
+            echo -ne "\033[0;31mFAILED\033[0m: Expected: $r, Got: $progr"
+        fi
     fi
     echo ""
 done
-    
-    
-    
